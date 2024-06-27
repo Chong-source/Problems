ae6829017a64cac8d00c73d72ac7788db85df6ec
Chong <chong.wan@mail.utoronto.ca>
2024-06-27 15:21:06 -0400

    Solved two sums

--- leetcode_problems_june.py
+++ leetcode_problems_june.py
@@ -1,3 +1,14 @@
 """This is a document that stores the leetcode solutions that I have"""
 
+
 # June 27. Two sums
+def two_sum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    for x in range(len(nums)):
+        for y in range(x + 1, len(nums)):
+            if nums[x] + nums[y] == target:
+                return [x, y]
